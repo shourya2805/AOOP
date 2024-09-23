501cd35ea6b1e74a95055109a1e11d8d25e01f14
shourya2805 <korrapatishourya@gmail.com>
2024-09-23 11:25:35 +0530

    Delete src directory

--- src/EligibilityChecker.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class EligibilityChecker {
-    public boolean checkEligibility(Employee employee) {
-        // Assuming eligibility is based on full-time status
-        return employee.isFullTime();
-    }
-}
